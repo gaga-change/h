478bccf09bd2816bbf9dc27cf402eb96c380bca2
严俊东 <1172482914@qq.com>
2017-05-15 23:01:12 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp'),
+    less = require('gulp-less'),
+    scss = require('gulp-scss'),
+    cssmin = require('gulp-minify-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber');
+gulp.task('scss', function () {
+    gulp.src(['src/**/*.scss', '!src/**/_*.scss'])
+        // .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        // .pipe(sourcemaps.init())
+        .pipe(scss())
+        // .pipe(cssmin())
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest('src'))
+});
+gulp.task('scssWatch', function () {
+    gulp.watch('src/**/*.scss', ['scss']); //当所有less文件发生改变时，调用testLess任务
+});
